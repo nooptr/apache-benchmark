51d4ea37bbb52ed732d45d18bf56adbd683c4c2e
Ngo Van Thang <thangnvbkhn@gmail.com>
2015-05-01 15:37:08 +0900

    fix dynamic

--- dynamic.php
+++ dynamic.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello world!\n";
+print "Hello world!</br>";
 
 $no = rand(1, 100);
 $no2 = rand(1, 100);
@@ -8,6 +8,6 @@ for ($xx=1; $xx<$no; $xx++) {
     for ($yy=1; $yy<$no2; $yy++) {
         printf("%4d",$xx*$yy);
     }
-    print "\n";
+    print "</br>";
 }
 ?>
